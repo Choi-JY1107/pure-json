92dd0caf047db304357d786f6199ae0b66307e2b
Choi-JY1107 <cjy11230@kakao.com>
2026-02-25 13:25:22 +0900

    fix: monaco에 TS 워커 출력

--- src/lib/monaco/index.ts
+++ src/lib/monaco/index.ts
@@ -1,6 +1,7 @@
 import * as monaco from "monaco-editor";
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
 let initialized = false;
 
@@ -12,6 +13,9 @@ export function initMonaco() {
     getWorker(_: string, label: string) {
       if (label === "json") {
         return new jsonWorker();
+      }
+      if (label === "typescript" || label === "javascript") {
+        return new tsWorker();
       }
       return new editorWorker();
     },
